6717e068db6a127f2935c78cbbbb9474acc75391
wilsonmar <wilsonmar+github@gmail.com>
2024-02-11 20:27:12 -0700

    v1.163 No curl -i for headers in mac-setup_env : mac-setup.zsh

--- mac-setup.zsh
+++ mac-setup.zsh
@@ -25,7 +25,7 @@ LOG_DATETIME=$( date +%Y-%m-%dT%H:%M:%S%z)-$((1 + RANDOM % 1000))  # 2023-09-21T
 EPOCH_START="$( date -u +%s )"  # such as 1572634619
 
 THIS_PROGRAM="${0##*/}" # excludes the ./ in "$0" 
-SCRIPT_VERSION="v1.161" # SCRIPT_VERSION download fix : mac-setup.zsh"
+SCRIPT_VERSION="v1.162" # Iterative check_mac-setup_env : mac-setup.zsh"
 # sudo password mac-setup.env init : mac-setup.zsh"
 # Identify latest https://github.com/balena-io/etcher/releases/download/v1.18.11/balenaEtcher-1.18.11.dmg from https://etcher.balena.io/#download-etcher
 # working github -aiac : mac-setup.zsh"
@@ -274,7 +274,7 @@ download_file_from_github(){
       fi
 
       h2 "Downloading \"${GITHUB_DOWNLOAD_URL}/$1\" ..."
-      curl -iLO "${GITHUB_DOWNLOAD_URL}/$1" 
+      curl -LO "${GITHUB_DOWNLOAD_URL}/$1" 
       echo "  "
       ls -ltaT "$ENV_FOLDERPATH/$1"
       return 0
