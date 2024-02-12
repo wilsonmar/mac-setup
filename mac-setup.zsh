574f0369ad907bcfede3e5c12b60a0c3b9f5ce5b
wilsonmar <wilsonmar+github@gmail.com>
2024-02-11 19:30:08 -0700

    v1.159 no download fix : mac-setup.zsh

--- mac-setup.zsh
+++ mac-setup.zsh
@@ -294,7 +294,7 @@ check_mac-setup_env(){
       h2 "Now please edit the file to customize variables ..."
       exit 9
    else
-      note "File not found"
+      note "File \"$ENV_FOLDERPATH/$1\" not found"
    fi
 }
 check_mac-setup_env "mac-setup.env"
