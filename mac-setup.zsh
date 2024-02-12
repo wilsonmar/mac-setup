01e8b08d56149c7f217a99b9db1ca94be1b512d1
wilsonmar <wilsonmar+github@gmail.com>
2024-02-11 18:59:29 -0700

    v1.156 curl -il to fix moved permanently :mac-setup.zsh

--- mac-setup.zsh
+++ mac-setup.zsh
@@ -25,7 +25,7 @@ LOG_DATETIME=$( date +%Y-%m-%dT%H:%M:%S%z)-$((1 + RANDOM % 1000))  # 2023-09-21T
 EPOCH_START="$( date -u +%s )"  # such as 1572634619
 
 THIS_PROGRAM="${0##*/}" # excludes the ./ in "$0" 
-SCRIPT_VERSION="v1.156" # curl -il fix moved permanently :mac-setup.zsh"
+SCRIPT_VERSION="v1.156" # curl -il to fix moved permanently :mac-setup.zsh"
 # sudo password mac-setup.env init : mac-setup.zsh"
 # Identify latest https://github.com/balena-io/etcher/releases/download/v1.18.11/balenaEtcher-1.18.11.dmg from https://etcher.balena.io/#download-etcher
 # working github -aiac : mac-setup.zsh"
@@ -275,8 +275,8 @@ download_file_from_github(){
          exit 9
       fi
 
-      h2 "Downloading file \"$ENV_FOLDERPATH/$1\" from \"${GITHUB_DOWNLOAD_URL}/$1\" ..."
-      curl --create-dirs -O --output-dir -iL $ENV_FOLDERPATH "${GITHUB_DOWNLOAD_URL}/$1" 
+      h2 "Downloading from \"${GITHUB_DOWNLOAD_URL}/$1\" ..."
+      curl -iL $ENV_FOLDERPATH "${GITHUB_DOWNLOAD_URL}/$1" 
       chmod +x "$ENV_FOLDERPATH/$1"
       ls -ltaT "$ENV_FOLDERPATH/$1"
       return 0
